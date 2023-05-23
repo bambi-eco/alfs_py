b80bb04ecdf1fc7ad456eba02ec6e710eadb3062
CleoChu <50024904+CleoChu@users.noreply.github.com>
2023-05-23 11:28:33 +0200

    Declaring first structure

---
+++ src/core/conv/coord_conversion.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+from pyrr import Vector3
+
+
+class PixelOrigin(Enum):
+    """
+    Enum representing a 2D coordinate origin.
+    The first two bits represent the vertical while the last two represent horizontal positioning:
+    - 00xx: top
+    - 01xx: vertical center
+    - 10xx: bottom
+    - xx00: left
+    - xx01: horizontal center
+    - xx10: right
+    """
+    TopLeft =      0b0000
+    TopCenter =    0b0001
+    TopRight =     0b0010
+
+    CenterLeft =   0b0100
+    Center =       0b0101
+    CenterRight =  0b0110
+
+    BottomLeft =   0b1000
+    BottomCenter = 0b1001
+    BottomRight =  0b1010
+
+
+def pixel_to_3d_coord(x: float, y: float, width: int, height: int, camera_position: Vector3, fovy: float,
+                      aspect_ratio: float, orthogonal: bool = False, distorted: bool = False, dist_coeffs = None) -> Vector3:
+    pass
